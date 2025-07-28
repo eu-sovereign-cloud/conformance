cc6771aa654e84ff6ff7551004c76cdff5662691
Glaucio Jannotti <iannotti.glaucio@gmail.com>
2025-07-28 16:58:28 -0300

    Formating issues

--- secatest/suite.go
+++ secatest/suite.go
@@ -7,7 +7,6 @@ import (
 )
 
 func requireNoError(sCtx provider.StepCtx, err error) {
-
 	sCtx.WithNewStep("Verify no error", func(stepCtx provider.StepCtx) {
 		stepCtx.WithNewParameters("error", fmt.Sprintf("%v", err))
 		stepCtx.Require().NoError(err, "Should not return an error")
@@ -15,7 +14,6 @@ func requireNoError(sCtx provider.StepCtx, err error) {
 }
 
 func requireNotNilResponse(sCtx provider.StepCtx, resp interface{}) {
-
 	sCtx.WithNewStep("Verify nil response", func(stepCtx provider.StepCtx) {
 		stepCtx.WithNewParameters("response", fmt.Sprintf("%v", resp))
 		stepCtx.Require().NotNil(resp, "Should be not nil")
