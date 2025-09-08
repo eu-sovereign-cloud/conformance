64626b1dca980323801b774efdd4256240072658
Glaucio Jannotti <glaucio.jannotti@ionos.com>
2025-09-08 15:34:04 -0300

    Removing unnecessary step parameters

--- secatest/steps.go
+++ secatest/steps.go
@@ -7,29 +7,6 @@ import (
 
 func verifyGlobalMetadataStep(ctx provider.StepCtx, expected *secalib.Metadata, actual *secalib.Metadata) {
 	ctx.WithNewStep("Verify metadata", func(stepCtx provider.StepCtx) {
-		stepCtx.WithNewParameters(
-			"expected_name", expected.Name,
-			"actual_name", actual.Name,
-
-			"expected_provider", expected.Provider,
-			"actual_provider", actual.Provider,
-
-			"expected_resource", expected.Resource,
-			"actual_resource", actual.Resource,
-
-			"expected_verb", expected.Verb,
-			"actual_verb", actual.Verb,
-
-			"expected_api_version", expected.ApiVersion,
-			"actual_api_version", actual.ApiVersion,
-
-			"expected_kind", expected.Kind,
-			"actual_kind", actual.Kind,
-
-			"expected_tenant", expected.Tenant,
-			"actual_tenant", actual.Tenant,
-		)
-
 		stepCtx.Require().Equal(expected.Name, actual.Name, "Name should match expected")
 		stepCtx.Require().Equal(expected.Provider, actual.Provider, "Provider should match expected")
 		stepCtx.Require().Equal(expected.Resource, actual.Resource, "Resource should match expected")
@@ -41,35 +18,6 @@ func verifyGlobalMetadataStep(ctx provider.StepCtx, expected *secalib.Metadata, 
 
 func verifyRegionalMetadataStep(ctx provider.StepCtx, expected *secalib.Metadata, actual *secalib.Metadata) {
 	ctx.WithNewStep("Verify metadata", func(stepCtx provider.StepCtx) {
-		stepCtx.WithNewParameters(
-			"expected_name", expected.Name,
-			"actual_name", actual.Name,
-
-			"expected_provider", expected.Provider,
-			"actual_provider", actual.Provider,
-
-			"expected_resource", expected.Resource,
-			"actual_resource", actual.Resource,
-
-			"expected_verb", expected.Verb,
-			"actual_verb", actual.Verb,
-
-			"expected_api_version", expected.ApiVersion,
-			"actual_api_version", actual.ApiVersion,
-
-			"expected_kind", expected.Kind,
-			"actual_kind", actual.Kind,
-
-			"expected_tenant", expected.Tenant,
-			"actual_tenant", actual.Tenant,
-
-			"expected_workspace", expected.Workspace,
-			"actual_workspace", actual.Workspace,
-
-			"expected_region", expected.Region,
-			"actual_region", actual.Region,
-		)
-
 		stepCtx.Require().Equal(expected.Name, actual.Name, "Name should match expected")
 		stepCtx.Require().Equal(expected.Provider, actual.Provider, "Provider should match expected")
 		stepCtx.Require().Equal(expected.Resource, actual.Resource, "Resource should match expected")
@@ -83,10 +31,6 @@ func verifyRegionalMetadataStep(ctx provider.StepCtx, expected *secalib.Metadata
 
 func verifyStatusStep(ctx provider.StepCtx, expected *secalib.Status, actual *secalib.Status) {
 	ctx.WithNewStep("Verify status", func(stepCtx provider.StepCtx) {
-		stepCtx.WithNewParameters(
-			"expected_state", expected.State,
-			"actual_state", actual.State,
-		)
 		stepCtx.Require().Equal(expected.State, actual.State, "State should match expected")
 	})
 }
